2fe4a263311818a09813b91e2abdb7853effbaae
briancmurphy87 <38481645+briancmurphy87@users.noreply.github.com>
2022-01-18 12:39:11 -0600

    Enh mock smockit (#1)
    * try using smockit to mock erc721
    
    * update unit tests to use mocking fixture from smockit library

--- test/doppelganger-test.js
+++ test/doppelganger-test.js
@@ -1,14 +1,15 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const { smock } = require('@defi-wonderland/smock');
 
 describe("Doppelganger", function () {
-  let mockContract, doppelgangerContract;
+  let myFake;
+  let doppelgangerContract;
 
   beforeEach(async function () {
-    // Original NFT contract
-    const ERC721Mock = await ethers.getContractFactory("ERC721Mock");
-    mockContract = await ERC721Mock.deploy();
-    await mockContract.deployed();
+    // Original NFT contract (mocked)
+    const myErc721Factory = await ethers.getContractFactory("ERC721");
+    myFake = await smock.fake(myErc721Factory);
 
     // Deploy the snapshot contract
     const Doppelganger = await ethers.getContractFactory("Doppelganger");
@@ -19,40 +20,33 @@ describe("Doppelganger", function () {
   it("cannot mint the snapshot if you are not the owner", async function () {
     [nftOwner, notNftOwner] = await ethers.getSigners();
 
-    // Mint the original NFT
-    const txn = await mockContract.connect(nftOwner).mint("http://google.com/nfts");
-    await txn.wait();
-
-    // Get the original NFT tokenId
-    let tokensCount = await mockContract.balanceOf(nftOwner.address)
-    const tokenId = await mockContract.tokenOfOwnerByIndex(nftOwner.address, tokensCount - 1);
+    // prime mock - owner addr of this token
+    const tokenId = 1;
+    myFake.ownerOf.returns(nftOwner.address);
 
     // Minting the snapshot should fail if you're not the owner
     await expect(
-      doppelgangerContract.connect(notNftOwner).snapshot(mockContract.address, tokenId)
+      doppelgangerContract.connect(notNftOwner).snapshot(myFake.address, tokenId)
     ).to.be.revertedWith("Doppelganger: only owner of original token can snapshot.");
   });
 
   it("can mint the snapshot if you are the owner", async function () {
     [nftOwner] = await ethers.getSigners();
 
-    // Mint the original NFT
-    const txn = await mockContract.connect(nftOwner).mint("http://google.com/nfts");
-    await txn.wait();
+    // prime mock - owner addr of this token
+    const tokenId = 1;
+    myFake.ownerOf.returns(nftOwner.address);
 
-    // Get the original NFT tokenId
-    let tokensCount = await mockContract.balanceOf(nftOwner.address)
-    let tokenId = await mockContract.tokenOfOwnerByIndex(nftOwner.address, tokensCount - 1);
-
-    // Minting the snapshot should fail if you're not the owner
-    const snapshotTxn = await doppelgangerContract.connect(nftOwner).snapshot(mockContract.address, tokenId);
+    // Minting the snapshot should work if you are the owner
+    const snapshotTxn = await doppelgangerContract.connect(nftOwner).snapshot(myFake.address, tokenId);
     snapshotTxn.wait();
 
     tokensCount = await doppelgangerContract.balanceOf(nftOwner.address)
     expect(tokensCount).to.equal(1);
 
-    tokenId = await doppelgangerContract.tokenOfOwnerByIndex(nftOwner.address, tokensCount - 1);
-    const tokenUri = await doppelgangerContract.tokenURI(tokenId);
+    const realizedTokenId = await doppelgangerContract.tokenOfOwnerByIndex(nftOwner.address, tokensCount - 1);
+    expect(realizedTokenId).to.equal(tokenId);
+    const tokenUri = await doppelgangerContract.tokenURI(realizedTokenId);
     expect(tokenUri).to.equal("blah");
   });
 });
